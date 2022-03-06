f8d35a9664243a7b2319ddd57bdd3d2146ac3f27
widigdacahya <rizkiwidigda@gmail.com>
2022-03-06 14:50:06 +0700

    thread_use_bgthread

--- BgThread/app/src/main/java/com/example/thebackgroundthread/MainActivity.kt
+++ BgThread/app/src/main/java/com/example/thebackgroundthread/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.thebackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.thebackgroundthread.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(mainBinding.root)
 
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        //[model block UI Thread / lagging]
+        /*
         mainBinding.btnStartUI.setOnClickListener {
             try {
                 //compressing simulation
@@ -29,6 +38,32 @@ class MainActivity : AppCompatActivity() {
                 }
             } catch (e: InterruptedException) {
                 e.printStackTrace()
+            }
+        }
+        */
+
+        //[model pake executer]
+        mainBinding.btnStartUI.setOnClickListener {
+            executor.execute {
+                try {
+                    //simulate process on background thread'
+                    for(i in 0..10) {
+                        Thread.sleep(500)
+                        val precentage = i*10
+
+                        //handler.post to make somethin on UI Thread
+                        handler.post {
+                            if(precentage==100) {
+                                mainBinding.tvStatusUI.text = getString(R.string.task_completed)
+                            } else {
+                                mainBinding.tvStatusUI.text = String.format(getString(R.string.compressing),precentage)
+                            }
+                        }
+
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
             }
         }
 
