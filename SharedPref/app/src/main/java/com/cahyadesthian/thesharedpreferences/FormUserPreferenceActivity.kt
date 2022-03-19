500be86ae9b7365f54421140b7781f0294a2183c
widigdacahya <rizkiwidigda@gmail.com>
2022-03-19 09:32:06 +0700

    usermodel_userpreference
    next step is to make activity that contain form to user input

---
+++ SharedPref/app/src/main/java/com/cahyadesthian/thesharedpreferences/FormUserPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.cahyadesthian.thesharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+    }
+}
---
+++ SharedPref/app/src/main/java/com/cahyadesthian/thesharedpreferences/UserPreference.kt
@@ -0,0 +1,41 @@
+package com.cahyadesthian.thesharedpreferences
+
+import android.content.Context
+
+internal class UserPreference(context: Context){
+
+    private val preference = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun setUser(value: UserModel) {
+        val editor = preference.edit()
+        editor.putString(NAME, value.name)
+        editor.putString(EMAIL, value.email)
+        editor.putInt(AGE, value.age)
+        editor.putString(PHONE_NUMBER, value.phoneNumber)
+        editor.putBoolean(LOVE_HER, value.isLove)
+        editor.apply()
+    }
+
+    fun getUser(): UserModel {
+        val model = UserModel()
+        model.name = preference.getString(NAME,"")
+        model.email = preference.getString(EMAIL, "")
+        model.age = preference.getInt(AGE,0)
+        model.phoneNumber = preference.getString(PHONE_NUMBER,"")
+        model.isLove = preference.getBoolean(LOVE_HER, false)
+
+        return model
+    }
+
+
+
+
+    companion object {
+        private const val PREFS_NAME = "user_pref"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+        private const val AGE = "age"
+        private const val PHONE_NUMBER = "phone"
+        private const val LOVE_HER = "isLove"
+    }
+}
