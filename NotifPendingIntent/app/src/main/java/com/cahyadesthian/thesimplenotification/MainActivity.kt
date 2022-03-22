17f0b1d26b61565498c260b6033f7a50775c4444
widigdacahya <rizkiwidigda@gmail.com>
2022-03-22 17:32:06 +0700

    pending Intent Notification
    in this session used to open web page or url

--- NotifPendingIntent/app/src/main/java/com/cahyadesthian/thesimplenotification/MainActivity.kt
+++ NotifPendingIntent/app/src/main/java/com/cahyadesthian/thesimplenotification/MainActivity.kt
@@ -2,8 +2,11 @@ package com.cahyadesthian.thesimplenotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,8 +34,19 @@ class MainActivity : AppCompatActivity() {
 
     //aksi untuk onCLick pada button
     fun sendNotification(view: View) {
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://portofolio.cahyadesthian.com/"))
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_MUTABLE else 0
+        )
+
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_circle_notifications_24))
             .setContentTitle(resources.getString(R.string.content_title))
@@ -60,9 +74,10 @@ class MainActivity : AppCompatActivity() {
 
             //create or update
             val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT)
-            channel.description = CHANNEL_NAME
+            //channel.description = CHANNEL_NAME
 
             mBuilder.setChannelId(CHANNEL_ID)
+
             mNotificationManager.createNotificationChannel(channel)
         }
 
