998680cb6f699193564234ec38b2b6f66f17be98
widigdacahya <rizkiwidigda@gmail.com>
2022-03-23 08:24:34 +0700

    Weather Button

--- SchedulerWorkManager/app/src/main/java/com/cahyadesthian/theworkmanager/MainActivity.kt
+++ SchedulerWorkManager/app/src/main/java/com/cahyadesthian/theworkmanager/MainActivity.kt
@@ -2,10 +2,47 @@ package com.cahyadesthian.theworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.cahyadesthian.theworkmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var workManager: WorkManager
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        workManager = WorkManager.getInstance(this)
+        mainBinding.btnOnetimetaskMainUI.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_onetimetask_mainUI -> startOneTimeTask()
+        }
+    }
+
+
+    private fun startOneTimeTask() {
+        mainBinding.tvTextStatusMainUI.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, mainBinding.edtCityMainUI.text.toString())
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this@MainActivity, { workInfo ->
+                val status = workInfo.state.name
+                mainBinding.tvTextStatusMainUI.append("\n" + status)
+            })
+    }
+
 }
