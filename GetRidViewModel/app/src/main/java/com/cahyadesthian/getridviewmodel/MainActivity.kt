4f28e1156e1455c5124a2567dc791a5fb5f4daab
widigdacahya <rizkiwidigda@gmail.com>
2022-03-27 13:34:14 +0700

    Get Rid Viewmodel DONE

--- GetRidViewModel/app/src/main/java/com/cahyadesthian/getridviewmodel/MainActivity.kt
+++ GetRidViewModel/app/src/main/java/com/cahyadesthian/getridviewmodel/MainActivity.kt
@@ -2,10 +2,43 @@ package com.cahyadesthian.getridviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.cahyadesthian.getridviewmodel.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+    lateinit var viewModel: TestViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        viewModel = ViewModelProvider(this).get(TestViewModel::class.java)
+        mainBinding.btnIncrement.setOnClickListener(this)
+
+        viewModel.currentNumber.observe(this, Observer {
+            mainBinding.tvCountData.text = it.toString()
+        })
+
+        viewModel.currentBoolean.observe(this, Observer {
+            mainBinding.tvBooleanData.text = it.toString()
+        })
+
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_increment -> {
+                viewModel.currentNumber.value = ++viewModel.number
+                viewModel.currentBoolean.value = viewModel.number %2 == 0
+            }
+        }
+    }
+
+
 }
---
+++ GetRidViewModel/app/src/main/java/com/cahyadesthian/getridviewmodel/TestViewModel.kt
@@ -0,0 +1,19 @@
+package com.cahyadesthian.getridviewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TestViewModel : ViewModel() {
+
+    var number = 0
+
+    val currentNumber : MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+    val currentBoolean: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+
+}
