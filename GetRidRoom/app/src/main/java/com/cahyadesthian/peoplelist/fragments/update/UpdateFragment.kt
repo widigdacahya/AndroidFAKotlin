8eba8b916e80489c216c46b9e87410d07ea35d58
widigdacahya <rizkiwidigda@gmail.com>
2022-03-27 21:32:09 +0700

    Data from list fragment to Update fragement
    Seems so magic, with args we just move data from list to soemthin like detail one in this case is update fragment, it still only bring the data, next gonna set button functionality. And also there is some way to set text from args to text in edit text such as use setText and Editable.Factory

--- GetRidRoom/app/src/main/java/com/cahyadesthian/peoplelist/fragments/update/UpdateFragment.kt
+++ GetRidRoom/app/src/main/java/com/cahyadesthian/peoplelist/fragments/update/UpdateFragment.kt
@@ -1,22 +1,38 @@
 package com.cahyadesthian.peoplelist.fragments.update
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.cahyadesthian.peoplelist.R
+import com.cahyadesthian.peoplelist.databinding.FragmentUpdateBinding
 
 
 class UpdateFragment : Fragment() {
 
+    private val args by navArgs<UpdateFragmentArgs>()
+    private lateinit var updateFragBinding: FragmentUpdateBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        //return inflater.inflate(R.layout.fragment_update, container, false)
+
+        updateFragBinding = FragmentUpdateBinding.inflate(inflater,container,false)
+
+        updateFragBinding.edtFirstnameFragUpdt.text = Editable.Factory.getInstance().newEditable(args.currentUser.firstName)
+        updateFragBinding.edtLastnameFragAdd.setText(args.currentUser.lastName)
+        updateFragBinding.edtAgeFragUpdt.setText(args.currentUser.age.toString())
+
+
+        return updateFragBinding.root
+
+
     }
 
 
