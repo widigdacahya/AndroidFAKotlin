a3913ad6568706022b07faaef094f3be15bd31bf
widigdacahya <rizkiwidigda@gmail.com>
2022-03-27 16:57:57 +0700

    Setup ActionBar title
    by navhost fragment-navcontroller

--- GetRidRoom/app/src/main/java/com/cahyadesthian/peoplelist/MainActivity.kt
+++ GetRidRoom/app/src/main/java/com/cahyadesthian/peoplelist/MainActivity.kt
@@ -2,10 +2,28 @@ package com.cahyadesthian.peoplelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.cahyadesthian.peoplelist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        //val navController: NavController = findNavController(R.id.fragmentContainerView)
+        //setupActionBarWithNavController(navController)
+
+        //setSupportActionBar(mainBinding.appBarMain.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+
     }
 }
