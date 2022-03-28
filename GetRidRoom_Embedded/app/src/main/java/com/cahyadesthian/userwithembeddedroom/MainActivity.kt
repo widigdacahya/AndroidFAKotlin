f57cc5b98fb556c708363f03b91f5a6c7869daeb
widigdacahya <rizkiwidigda@gmail.com>
2022-03-28 17:33:10 +0700

    MainActivity DONE
    integrating adapter,database,dao,eall of previous thing in MainACtivity, it insert data on this activity by craeting or code it inside mainActivity.kt

--- GetRidRoom_Embedded/app/src/main/java/com/cahyadesthian/userwithembeddedroom/MainActivity.kt
+++ GetRidRoom_Embedded/app/src/main/java/com/cahyadesthian/userwithembeddedroom/MainActivity.kt
@@ -2,10 +2,44 @@ package com.cahyadesthian.userwithembeddedroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cahyadesthian.userwithembeddedroom.data.Address
+import com.cahyadesthian.userwithembeddedroom.data.Person
+import com.cahyadesthian.userwithembeddedroom.data.PersonViewModel
+import com.cahyadesthian.userwithembeddedroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    val adapter by lazy { PersonAdapter()  }
+    private lateinit var personViewModel: PersonViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        personViewModel = ViewModelProvider(this).get(PersonViewModel::class.java)
+
+        mainBinding.apply {
+            rcPerson.adapter = adapter
+            rcPerson.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        }
+
+
+        /*
+        * Insert Data Person and Address
+        * */
+        val addressToInsert = Address("Keputih gg 1C Surabaya Sukolilo", 99999)
+        val personToInsert = Person(0, "Nareswara", "Garuda", 109,addressToInsert)
+        personViewModel.insertPerson(personToInsert)
+
+        personViewModel.readPerson.observe(this, {
+            adapter.setData(it)
+        })
+
     }
 }
