5551a41dbbb6f200061ad1baa735ce7b4d0c353b
widigdacahya <rizkiwidigda@gmail.com>
2022-03-28 16:12:21 +0700

    Create Dao
    Data Access Object for Person database, there is query to read all data persons in a list and insert with conflict strategy replace

---
+++ GetRidRoom_Embedded/app/src/main/java/com/cahyadesthian/userwithembeddedroom/data/PersonDao.kt
@@ -0,0 +1,18 @@
+package com.cahyadesthian.userwithembeddedroom.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface PersonDao {
+
+    @Query("SELECT * FROM person_table ORDER BY id ASC")
+    fun readPerson(): LiveData<List<Person>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPerson(person: Person)
+
+}
